e84705fff7c9cf6c3ebfffd6e772d157c0557a4e
yueming <bluemoon_cn@hotmail.com>
2023-10-25 15:01:00 +0200

    pics

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,7 +3,7 @@ import Layout from "../layout/Layout";
 import { createBrowserRouter } from "react-router-dom";
 import Role from "../pages/Role";
 import { AuthenticationGuard } from "../auth0/AuthenticationGuard";
-import Home from "../pages/Home";
+import Home from '../pages/Home';
 
 
 
